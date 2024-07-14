bdb8ba46a66be89588f736e1d7c14791ad80d0cb
miim1 <miim1@DESKTOP-07JOVA5>
2024-07-14 20:46:36 +0900

    first commit

---
+++ ROUTINEUP/src/main/java/com/example/ROUTINEUP/RoutineupApplication.java
@@ -0,0 +1,13 @@
+package com.example.ROUTINEUP;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RoutineupApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RoutineupApplication.class, args);
+	}
+
+}
---
+++ ROUTINEUP/src/test/java/com/example/ROUTINEUP/RoutineupApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ROUTINEUP;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RoutineupApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
